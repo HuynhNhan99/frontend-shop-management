0ca19786d1254f8bf9b9c8949248614f369ae00c
Nhan Huynh <httnhan99@gmail.com>
2025-11-04 14:10:58 +0700

    Thêm tính năng PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Shop Management',
+        short_name: 'Shop',
+        theme_color: '#00b894',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })],
   server: { port: 5173 }
 })
